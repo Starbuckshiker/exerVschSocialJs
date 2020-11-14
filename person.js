5fc1bd209acb435c6811bf2545070c92c7934ddc
starbuckshiker <u1304954@umail.utah.edu>
2020-11-13 21:09:34 -0700

    that was intersting

---
+++ person.js
@@ -0,0 +1,62 @@
+var person = {
+    name: "Rick",
+    age: 70,
+    friends: {
+        Name: "Morty",
+        age: 55,
+        address: {
+            street: "your couch",
+            city: "your living room"
+        },
+        likesBeer: "Yes",
+        hasAcar: {
+            car: "yes",
+            type: "Honda",
+            doors: "four"
+        }
+
+    },
+        Name: "Sue",
+        age: 63,
+        address: {
+            street: "spare room",
+            city: "you have a big house"
+        },
+        likesBeer: "no",
+        hasAcar: {
+            car: "no",
+            type: "",
+            doors: ""
+        },
+        Name: "Bob",
+        age: 85,
+        address: {
+            street: "the other spare room",
+            city: "how many rooms you got"
+        },
+        likesBeer: "Yes",
+        hasAcar: {
+            car: "yes",
+            type: "Ford",
+            doors: "four"
+        },
+        Name: "Joe",
+        age: 55,
+        address: {
+            street: "your couch",
+            city: "your living room"
+        },
+        likesBeer: "Yes",
+    address: {
+        street: "123 street",
+        city: "Somewhere"
+    },
+    likesBeer: "Yes",
+    pets: ["dog", "cat", "hamster", "falcon"]
+}
+person.friends.Name = "Sam";
+person['sex'] = "";
+person.pets.push("rabbit", "cow", "chicken");
+alert(person.pets);
+console.log(person.pets);
+console.log(person.friends)
